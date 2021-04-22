7e5d9986394b17f172f524edd2a4f222433b16e0
James McMullan <mcmullan.jgh@gmail.com>
2021-04-22 03:37:43 +0100

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,4 +1,17 @@
-puts "Please enter the names of the students"
+def interactive_menu
+    students = []
+    loop do
+      # 1. print the menu and ask the user what to do
+      puts "1. Input the students"
+      puts "2. Show the students"
+      puts "9. Exit" # 9 because we'll be adding more items
+      # 2. read the input and save it into a variable
+      selection = gets.chomp
+      # 3. do what the user has asked
+      case selection
+      when "1"
+        # students = input_students
+        puts "Please enter the names of the students"
 puts "To finish, just hit return twice"
 # empty array
 students = []
@@ -12,27 +25,23 @@ while !name.empty? do
   # get another name from the user
   name = gets.chomp
 end
-# return the array of students
-
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-  end
-  def print(students)
-      students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-      end
-    end
-  def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
-  end
-  #call methods
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-def print(students)
+      when "2"
+        def print_header
+            puts "The students of Villains Academy"
+            puts "-------------"
+          end
+          def print(students)
+            students.each do |student|
+              puts "#{student[:name]} (#{student[:cohort]} cohort)"
+            end
+          end
+        def print_footer(names)
+          puts "Overall, we have #{names.count} great students"
+        end
+        print_header
+        print(students)
+        print_footer(students)
+        def print(students)
     students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
@@ -40,7 +49,23 @@ def print(students)
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-#call methods
-print_header
-print(students)
-print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+      end
+    end
+  end
+
+interactive_menu do
+# return the array of students
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+end
+
+
+      
